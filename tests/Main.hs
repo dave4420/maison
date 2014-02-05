c9390de0a0bb9e7bbb0e33c0984659f46163d241
Dave Hinton <beakerchu@googlemail.com>
2014-02-05 17:24:04 +0000

    Added tests for `singleSite`.

--- tests/Main.hs
+++ tests/Main.hs
@@ -9,6 +9,13 @@ import           Data.Monoid
 import           Data.ByteString               (ByteString)
 import qualified Data.ByteString        as B
 import qualified Data.ByteString.Char8  as BC
+import qualified Data.ByteString.Lazy   as BL
+
+-- http-types
+import qualified Network.HTTP.Types     as HTTP
+
+-- lens
+import           Control.Lens.Operators
 
 -- QuickCheck
 import           Test.QuickCheck
@@ -25,11 +32,16 @@ import           Test.Framework.Providers.QuickCheck2
 -- test-framework-th
 import           Test.Framework.TH
 
+-- text
+import qualified Data.Text              as T
+import qualified Data.Text.Encoding     as T
+
 -- wai
 import qualified Network.Wai            as WAI
 
 -- wai-test
-import           Network.Wai.Test
+import           Network.Wai.Test              (Session)
+import qualified Network.Wai.Test       as WAI
 
 
 main :: IO ()
@@ -42,7 +54,8 @@ tests = $testGroupGenerator
 
 testSites :: Sites -> Session () -> Property
 testSites sites session
-        = propertyIO $ runSession session $ waiApplicationFromSitesForHttp sites
+        = propertyIO . WAI.runSession session
+          $ waiApplicationFromSitesForHttp sites
 
 hostnames :: Gen ByteString
 hostnames = B.intercalate "." <$> listOf1 segments
@@ -66,6 +79,26 @@ segments = BC.pack <$> listOf1 (elements alphabet) where
         alphabet = ['a'..'z'] ++ ['A'..'Z'] ++ ['0'..'9']
 
 
+setHost :: ByteString -> WAI.Request -> WAI.Request
+setHost host request = request {WAI.requestHeaderHost = Just host,
+                                WAI.requestHeaders = headers}
+    where
+        headers = ("Host", host) : WAI.requestHeaders request
+
+
+reflectUrl :: ByteString -> Site
+reflectUrl host = Site $ \path query -> let
+        entity = entityFromStrictByteString "content/type" bs
+        bs = mconcat [
+                "//",
+                host,
+                "/",
+                T.encodeUtf8 . T.intercalate "/" . F.toList $ path,
+                HTTP.renderQuery True query]
+    in (return . Right . (existingGet .~ Just (return entity)))
+       (defaultExistingResource :: ExistingResource)
+
+
 prop_emptySiteGET :: Property
 prop_emptySiteGET
         = forAll hostnames $ \hostname ->
@@ -73,9 +106,40 @@ prop_emptySiteGET
           forAll queries $ \query ->
           testSites mempty $ do
                 response
-                  <- request
-                     . (`setPath` (path <> query))
-                     $ WAI.defaultRequest {
-                        WAI.requestMethod = "GET",
-                        WAI.requestHeaders = [("Host", hostname)]}
-                assertStatus 400 response
+                 <- WAI.request
+                    . (`WAI.setPath` (path <> query))
+                    . setHost hostname
+                    $ WAI.defaultRequest {WAI.requestMethod = "GET"}
+                WAI.assertStatus 400 response
+
+
+prop_singleSiteHitGET :: Property
+prop_singleSiteHitGET
+        = forAll hostnames $ \hostname ->
+          forAll paths $ \path ->
+          forAll queries $ \query ->
+          testSites (singleSite hostname $ reflectUrl hostname) $ do
+                response
+                 <- WAI.request
+                    . (`WAI.setPath` (path <> query))
+                    . setHost hostname
+                    $ WAI.defaultRequest {WAI.requestMethod = "GET"}
+                WAI.assertStatus 200 response
+                WAI.assertBody (BL.fromStrict
+                                $ mconcat ["//", hostname, path, query])
+                               response
+
+
+prop_singleSiteMissGET :: Property
+prop_singleSiteMissGET
+        = forAll hostnames $ \hostname ->
+          forAll hostnames $ \hostname' ->
+          forAll paths $ \path ->
+          forAll queries $ \query ->
+          testSites (singleSite hostname' $ reflectUrl hostname) $ do
+                response
+                 <- WAI.request
+                    . (`WAI.setPath` (path <> query))
+                    . setHost hostname
+                    $ WAI.defaultRequest {WAI.requestMethod = "GET"}
+                WAI.assertStatus 400 response
